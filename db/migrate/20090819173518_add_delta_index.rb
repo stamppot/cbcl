1d277e41ff4b8cb71f7265de3635e1218b2430e0
Jens Rasmussen <stamppot@gmail.com>
2009-08-19 19:58:17 +0200

    delta index migration

---
+++ db/migrate/20090819173518_add_delta_index.rb
@@ -0,0 +1,9 @@
+class AddDeltaIndex < ActiveRecord::Migration
+  def self.up
+    add_column :groups, :delta, :boolean, :default => true, :null => false
+  end
+
+  def self.down
+    remove_column :groups, :delta
+  end
+end
