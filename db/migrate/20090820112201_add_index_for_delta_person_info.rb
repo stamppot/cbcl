f331e9a39206e22d02838b02e85819040753e5df
Jens Rasmussen <stamppot@gmail.com>
2009-08-20 13:23:10 +0200

    delta for person infos

---
+++ db/migrate/20090820112201_add_index_for_delta_person_info.rb
@@ -0,0 +1,11 @@
+class AddIndexForDeltaPersonInfo < ActiveRecord::Migration
+  def self.up
+    add_index :person_infos, :delta, :boolean, :default => true, :null => false
+    add_index :person_infos, :delta
+  end
+
+  def self.down
+    remove_index :person_infos, :delta
+    remove_index :person_infos, :delta
+  end
+end
