3342373ca115f0e636b6c43da8e72b597d802a7a
Jens Rasmussen <stamppot@gmail.com>
2009-11-28 00:48:21 +0100

    deactivated super_exception_notifier

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,20 +28,20 @@ config.action_view.cache_template_loading = true
 
 # http://code.google.com/p/super-exception-notifier/
 # defaults to exception.notifier@default.com
-ExceptionNotifier.sender_address =
-  %("CBCL Application Error" <stamppot@gmail.com>)
+# ExceptionNotifier.sender_address =
+#   %("CBCL Application Error" <stamppot@gmail.com>)
 
 # defaults to "[ERROR] "
-ExceptionNotifier.email_prefix = "[APP-#{RAILS_ENV.capitalize} ERROR] "
+# ExceptionNotifier.email_prefix = "[APP-#{RAILS_ENV.capitalize} ERROR] "
 
 #defaults to false - meaning by default it sends email.  Setting true will cause it to only render the error pages, and NOT email.
-ExceptionNotifier.render_only = false
+# ExceptionNotifier.render_only = false
 
 #defaults to %W( 405 500 503 )
-ExceptionNotifier.send_email_error_codes = %W( 400 403 404 405 410 500 501 503 )
+# ExceptionNotifier.send_email_error_codes = %W( 400 403 404 405 410 500 501 503 )
 
 #     defaults to: %("Exception Notifier" <exception.notifier@default.com>)
-ExceptionNotifier.sender_address = %("CBCL #{RAILS_ENV.capitalize} Error" <errors@cbcl-sdu.dk>)
+# ExceptionNotifier.sender_address = %("CBCL #{RAILS_ENV.capitalize} Error" <errors@cbcl-sdu.dk>)
 
 #defaults explained further down in detail
 # ExceptionNotifier.view_path = 'app/views/error'
