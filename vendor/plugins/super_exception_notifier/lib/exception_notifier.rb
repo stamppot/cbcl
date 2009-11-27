446890f287e96b67f66801b04af588a237c0c284
Jens Rasmussen <stamppot@gmail.com>
2009-11-28 00:44:20 +0100

    removed exception_notifier, added super_exception_notifier

--- vendor/plugins/super_exception_notifier/lib/exception_notifier.rb
+++ vendor/plugins/super_exception_notifier/lib/exception_notifier.rb
@@ -1,6 +1,7 @@
 require 'pathname'
 
 # Copyright (c) 2005 Jamis Buck
+# Portions Copyright (c) 2008 Peter Boling
 #
 # Permission is hereby granted, free of charge, to any person obtaining
 # a copy of this software and associated documentation files (the
@@ -21,21 +22,51 @@ require 'pathname'
 # OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 # WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 class ExceptionNotifier < ActionMailer::Base
-  @@sender_address = %("Exception Notifier" <exception.notifier@default.com>)
+  @@sender_address = %("#{RAILS_ENV.capitalize} Error" <errors@default.com>)
   cattr_accessor :sender_address
 
   @@exception_recipients = []
   cattr_accessor :exception_recipients
 
-  @@email_prefix = "[ERROR] "
+  @@email_prefix = "[#{RAILS_ENV.capitalize} ERROR] "
   cattr_accessor :email_prefix
 
   @@sections = %w(request session environment backtrace)
   cattr_accessor :sections
 
+  @@render_only = false
+  cattr_accessor :render_only
+
+  @@view_path = nil
+  cattr_accessor :view_path
+
+  #Emailed Error Notification will be sent if the error code matches one of the following error codes
+  @@send_email_error_codes = %W( 405 500 503 )
+  cattr_accessor :send_email_error_codes
+
+  #Emailed Error Notification will be sent if the error class matches one of the following error error classes
+  @@send_email_error_classes = %W( )
+  cattr_accessor :send_email_error_classes
+
   self.template_root = "#{File.dirname(__FILE__)}/../views"
 
   def self.reloadable?() false end
+
+  def self.get_view_path(status_cd)
+    if File.exist?("#{RAILS_ROOT}/public/#{status_cd}.html")
+      "#{RAILS_ROOT}/public/#{status_cd}.html"
+    elsif !view_path.nil? && File.exist?("#{RAILS_ROOT}/#{view_path}/#{status_cd}.html")
+      "#{RAILS_ROOT}/#{view_path}/#{status_cd}.html"
+    elsif File.exist?("#{RAILS_ROOT}/vendor/plugins/super_exception_notifier/views/exception_notifiable/#{status_cd}.html")
+      "#{RAILS_ROOT}/vendor/plugins/super_exception_notifier/views/exception_notifiable/#{status_cd}.html"
+    else 
+      "#{RAILS_ROOT}/vendor/plugins/super_exception_notifier/views/exception_notifiable/500.html"
+    end
+  end
+
+  def self.should_send_email?(status_cd, exception)
+    !self.render_only && (self.send_email_error_codes.include?(status_cd) || self.send_email_error_classes.include?(exception))
+  end
 
   def exception_notification(exception, controller, request, data={})
     content_type "text/plain"
