c601aa9f4f3f3fff203d8ac3724aa21b14515db6
Jens Rasmussen <stamppot@gmail.com>
2010-02-01 22:53:59 +0100

    update ratings count

--- db/migrate/20100111213750_add_no_ratings_caching.rb
+++ db/migrate/20100111213750_add_no_ratings_caching.rb
@@ -2,11 +2,11 @@ class AddNoRatingsCaching < ActiveRecord::Migration
   def self.up
     add_column :questions, :ratings_count, :int
     
-    # Question.all.each do |q|
-    #   q.ratings_count = q.ratings.size
-    #   q.save
-    # end
-
+    Question.all.each do |q|
+          q.ratings_count = q.ratings.size
+          q.save
+        end
+    
     add_column :answers, :ratings_count, :int # answered ratings count
     
     # unless ENV['RAILS_ENV'] == 'test'
@@ -21,5 +21,7 @@ class AddNoRatingsCaching < ActiveRecord::Migration
   end
 
   def self.down
+    remove_column :answers, :ratings_count
+    remove_column :questions, :ratings_count
   end
 end
