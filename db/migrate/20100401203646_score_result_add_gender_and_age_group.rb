6e483a487e14335826339f4add8d279ff285b88b
Jens Rasmussen <stamppot@gmail.com>
2010-04-12 01:24:09 +0200

    move age to score_rapport

--- db/migrate/20100401203646_score_result_add_gender_and_age_group.rb
+++ db/migrate/20100401203646_score_result_add_gender_and_age_group.rb
@@ -2,7 +2,7 @@ class ScoreResultAddGenderAndAgeGroup < ActiveRecord::Migration
   def self.up
     add_column :score_rapports, :gender, :integer, :null => false
     add_column :score_rapports, :age_group, :string, :limit => 5, :null => false
-    add_column :score_results, :missing, :integer, :default => 0
+    # add_column :score_results, :missing, :integer, :default => 0
     # remove_column :score_results, :percentile
     # 
     ScoreRapport.find_each {|sr| s = sr.score_results.first; sr.gender = s.gender; sr.age_group = s.age_group; sr.save}
@@ -10,7 +10,7 @@ class ScoreResultAddGenderAndAgeGroup < ActiveRecord::Migration
 
   def self.down
     # remove_column :score_results, :missing
-    remove_column :score_results, :age_group
-    remove_column :score_results, :gender
+    # remove_column :score_results, :age_group
+    # remove_column :score_results, :gender
   end
 end
