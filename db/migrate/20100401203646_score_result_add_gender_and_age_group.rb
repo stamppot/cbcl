12432967918182c1f8e826500a005a83806961b1
Jens Rasmussen <stamppot@gmail.com>
2010-04-12 01:25:56 +0200

    move age to score_rapport

--- db/migrate/20100401203646_score_result_add_gender_and_age_group.rb
+++ db/migrate/20100401203646_score_result_add_gender_and_age_group.rb
@@ -2,10 +2,16 @@ class ScoreResultAddGenderAndAgeGroup < ActiveRecord::Migration
   def self.up
     add_column :score_rapports, :gender, :integer, :null => false
     add_column :score_rapports, :age_group, :string, :limit => 5, :null => false
+    add_column :score_rapports, :age, :integer
+    
+    ScoreRapport.find_each(:include => [{:survey_answer => {:journal => :person_info} }]) do |sr|
+      s = sr.score_results.first; sr.gender = s.gender; sr.age_group = s.age_group
+      sr.age = sr.survey_answer.journal.person_info.age; sr.save
+    end
     # add_column :score_results, :missing, :integer, :default => 0
     # remove_column :score_results, :percentile
     # 
-    ScoreRapport.find_each {|sr| s = sr.score_results.first; sr.gender = s.gender; sr.age_group = s.age_group; sr.save}
+    # ScoreRapport.find_each {|sr| s = sr.score_results.first; sr.gender = s.gender; sr.age_group = s.age_group; sr.save}
   end
 
   def self.down
