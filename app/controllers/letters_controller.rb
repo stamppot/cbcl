a5a4abb581db2a9e236da6cd877f1b76b36a2eac
Jens Rasmussen <stamppot@gmail.com>
2010-08-14 16:25:59 +0200

    tiny hammer

--- app/controllers/letters_controller.rb
+++ app/controllers/letters_controller.rb
@@ -2,32 +2,17 @@ class LettersController < ApplicationController
   # layout 'cbcl', :except => [:new, :edit]
   layout 'wysiwyg' #, :only => [:new, :edit]
   
-  # GET /letters
-  # GET /letters.xml
   def index
     @letters = Letter.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render :xml => @letters }
-    end
   end
 
-  # GET /letters/1
-  # GET /letters/1.xml
   def show
     @letter = Letter.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @letter }
-    end
   end
 
-  # GET /letters/new
-  # GET /letters/new.xml
   def new
     @letter = Letter.new
+    @tinymce_hammer_required == true
     @groups = if params[:id]
       Group.find([params[:id]]).map { |g| [g.title, g.id] }
     elsif Letter.first(:conditions => ['group_id IS NULL']).nil? && current_user.admin? # no letters
@@ -40,33 +25,24 @@ class LettersController < ApplicationController
   # GET /letters/1/edit
   def edit
     @letter = Letter.find(params[:id])
+    @tinymce_hammer_required == true
     @groups = current_user.center_and_teams.map {|g| [g.title, g.id] }
     @groups.unshift ["Alle grupper", nil] if current_user.admin?
   end
 
-  # POST /letters
-  # POST /letters.xml
   def create
     @group = Group.find_by_id params[:letter][:group_id]
     @letter = Letter.new(params[:letter])
     @letter.group = @group
     
     render :new and return unless @letter.save
-    # if @letter.save
     flash[:notice] = 'Brevet er oprettet.'
     redirect_to(@letter)
-    # else
-    #   render :new
-    # end
   end
 
-  # PUT /letters/1
-  # PUT /letters/1.xml
   def update
     @letter = Letter.find(params[:id])
-     # params[:letter][:group] = 0 if params[:letter][:group].empty?
     params[:letter][:group_id] = nil if params[:letter][:group_id].blank?
-    # @letter.group_id = 0 if params[:letter]
 
     if @letter.update_attributes(params[:letter])
       flash[:notice] = 'Brevet er rettet.'
@@ -76,19 +52,11 @@ class LettersController < ApplicationController
     end
   end
 
-  # DELETE /letters/1
-  # DELETE /letters/1.xml
   def destroy
     @letter = Letter.find(params[:id])
     @letter.destroy
-
-    respond_to do |format|
-      format.html { redirect_to(letters_url) }
-      format.xml  { head :ok }
-    end
   end
   
-  # consider moving to letters_controller
   def show_login
     @entry = JournalEntry.find(params[:id], :include => :login_user)
     @login_user = @entry.login_user
