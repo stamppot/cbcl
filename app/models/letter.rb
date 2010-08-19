1d5698a5df5537658f99a1643d1425707671cb05
Jens Rasmussen <stamppot@gmail.com>
2010-08-19 17:57:51 +0200

    letters fix

--- app/models/letter.rb
+++ app/models/letter.rb
@@ -5,7 +5,7 @@ class Letter < ActiveRecord::Base
   validates_presence_of :letter
   validates_presence_of :name
   validates_presence_of :surveytype
-  validates_uniqueness_of :group, :scope => :surveytype
+  # validates_uniqueness_of :surveytype, :scope => :group
   
   def self.find_default(roletype)
     Letter.find(:first, :conditions => ['group_id IS NULL or group_id = ? AND surveytype = ?', 0, roletype] )
