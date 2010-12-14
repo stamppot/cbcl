1758e3648fc0739af8b0fdfaa4ae95cbe516a05b
Jens Rasmussen <stamppot@gmail.com>
2010-12-14 18:39:35 +0100

    vendored rails 2.3.5

--- config/environment.rb
+++ config/environment.rb
@@ -6,7 +6,7 @@
 ENV['RAILS_ENV'] ||= 'production'
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-# RAILS_GEM_VERSION = '2.3.3'
+# RAILS_GEM_VERSION = '2.3.5'
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
