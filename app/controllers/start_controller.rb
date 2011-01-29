3330e06e713641becb13ba733ebd5c914d28c4f0
Jens Rasmussen <stamppot@gmail.com>
2011-01-29 16:09:08 +0100

    pdf css fixes

--- app/controllers/start_controller.rb
+++ app/controllers/start_controller.rb
@@ -23,33 +23,15 @@ class StartController < ApplicationController
 	# end
 
 	def finish
-		puts "START_CONTROLLER #{session[:rbac_user_id]}"
-    
-		# if request.post?
-		puts "FINISH 1 #{params.inspect}  current_user: #{current_user.inspect} session: #{session[:rbac_user_id].inspect}"
 		# if session[:rbac_user_id] && (journal_entry = cookies[:journal_entry])  # TODO: put in helper method
-		params[:id] ||= cookies[:journal_entry] # login user can access survey with survey_id instead of journal_entry_id
+		# params[:id] ||= cookies[:journal_entry] # login user can access survey with survey_id instead of journal_entry_id
 		puts "FINISH 2 #{params.inspect}"
 		@journal_entry = JournalEntry.find(params[:id])
 		puts "FINISH 3: #{@journal_entry.login_user.inspect}  journal_entry: #{@journal_entry.inspect}"
 		# @journal_entry.login_user.destroy if @journal_entry.login_user
 		@survey_type = @journal_entry.survey.surveytype
+		cookies.delete "journal_entry"
 		puts "FINISH 4"
-		# if @survey_type == "youth"
-		# 	flash[:notice] = "Tak for dit svar!"
-		# 	puts "YOUTH FINISH 5"
-		# 	cookies.delete :journal_entry
-		# 	cookies.delete :user_name
-		# 	puts "FINISH 6"
-		# 	self.remove_user_from_session!
-		# 	puts "FINISH 7"
-		# 	redirect_to login_path and return
-		# end
-		puts "FINISH 8"
-		# end
-		# cookies.delete :journal_entry
-		# cookies.delete :user_name
-		puts "FINISH 9"
 	end
 
 	def check_logged_in
