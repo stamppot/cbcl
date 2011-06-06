2082a1a86f5dadd43ff7b75b4c69bb2f4b1b8bc5
Jens Rasmussen <stamppot@gmail.com>
2011-06-06 21:18:10 +0200

    removed obsoleted migrations, added code book migration

--- db/migrate/20100204153251_create_index_periods.rb
+++
@@ -1,9 +0,0 @@
-class CreateIndexPeriods < ActiveRecord::Migration
-  def self.up
-    add_index :periods, :subscription_id
-  end
-
-  def self.down
-    remove_index :periods, :subscription_id
-  end
-end
--- db/migrate/20101211173347_add_user_delta.rb
+++
@@ -1,9 +0,0 @@
-class AddUserDelta < ActiveRecord::Migration
-  def self.up
-		add_column :users, :delta, :integer
-  end
-
-  def self.down
-		remove_column :users, :delta
-  end
-end
