d9cc298fef3b1c09c29833fba6bff19dd5276aee
Jens Rasmussen <stamppot@gmail.com>
2011-11-02 22:23:48 +0100

    try to fix start is pdf-page

--- app/controllers/start_controller.rb
+++ app/controllers/start_controller.rb
@@ -6,7 +6,6 @@ class StartController < ApplicationController
 		@journal_entry = JournalEntry.find_by_user_id(current_user.id)
 		redirect_to login_path and return if @journal_entry.nil?
 		@survey = @journal_entry.survey
-		# render :edit if @journal_entry.survey_answer
 	end
 
 	def edit
@@ -16,22 +15,10 @@ class StartController < ApplicationController
 		@survey = @journal_entry.survey
 	end
 
-	# def finish
-	#   login_user = LoginUser.find_by_id(params[:id])
-	#   # login_user.destroy if login_user
-	#   remove_current_user
-	# end
-
 	def finish
-		# if session[:rbac_user_id] && (journal_entry = cookies[:journal_entry])  # TODO: put in helper method
-		# params[:id] ||= cookies[:journal_entry] # login user can access survey with survey_id instead of journal_entry_id
-		puts "FINISH 1 params: #{params.inspect}"
-		@journal_entry = JournalEntry.find(params[:id])
-		puts "FINISH 3: #{@journal_entry.login_user.inspect}  journal_entry: #{@journal_entry.inspect}"
-		# @journal_entry.login_user.destroy if @journal_entry.login_user
-		@survey_type = @journal_entry.survey.surveytype
+		@journal_entry = JournalEntry.find_by_user_id(current_user.id)
+    # @survey_type = @journal_entry.survey.surveytype
 		cookies.delete "journal_entry"
-		puts "FINISH 4"
 	end
 
 	def check_logged_in
