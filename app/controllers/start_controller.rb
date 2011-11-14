b39ddfddca7f1738b5ba818a88296bd1779b0fe0
Jens Rasmussen <stamppot@gmail.com>
2011-11-14 23:37:50 +0100

    start_controller fixje

--- app/controllers/start_controller.rb
+++ app/controllers/start_controller.rb
@@ -1,6 +1,7 @@
 class StartController < ApplicationController
 
   def start
+    @journal_entry = JournalEntry.find_by_user_id(current_user.id)
     logger.info "LOGIN_USER start cookie: #{cookies[:journal_entry]} #{@journal_entry.id} #{@journal_entry.login_user.id} @ #{9.hours.from_now.to_s(:short)}: #{request.env['HTTP_USER_AGENT']}"
     redirect_to continue_path if @journal_entry.draft?
     redirect_to survey_finish_path(@journal_entry) and return if @journal_entry.answered?
@@ -8,6 +9,7 @@ class StartController < ApplicationController
   end
 
   def continue
+    @journal_entry = JournalEntry.find_by_user_id(current_user.id)
     logger.info "LOGIN_USER continue cookie: #{cookies[:journal_entry]} #{@journal_entry.id} #{@journal_entry.login_user.id} @ #{9.hours.from_now.to_s(:short)}: #{request.env['HTTP_USER_AGENT']}"
     @survey = @journal_entry.survey
   end
@@ -17,6 +19,7 @@ class StartController < ApplicationController
   # end
 
   def finish
+    @journal_entry = JournalEntry.find_by_user_id(current_user.id)
     redirect_to survey_continue_path(@journal_entry) and return unless @journal_entry.answered?
     @survey = @journal_entry.survey
     # @survey_type = @journal_entry.survey.surveytype
