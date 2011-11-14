fc70147e0ee28f9fc295e18db7734a000e75ec2f
Jens Rasmussen <stamppot@gmail.com>
2011-11-15 00:05:51 +0100

    save_draft, continue path

--- app/controllers/start_controller.rb
+++ app/controllers/start_controller.rb
@@ -3,7 +3,7 @@ class StartController < ApplicationController
   def start
     @journal_entry = JournalEntry.find_by_user_id(current_user.id)
     logger.info "LOGIN_USER start cookie: #{cookies[:journal_entry]} #{@journal_entry.id} #{@journal_entry.login_user.id} @ #{9.hours.from_now.to_s(:short)}: #{request.env['HTTP_USER_AGENT']}"
-    redirect_to continue_path if @journal_entry.draft?
+    redirect_to survey_continue_path if @journal_entry.draft?
     redirect_to survey_finish_path(@journal_entry) and return if @journal_entry.answered?
     @survey = @journal_entry.survey
   end
