9e252ab470d37e935aaa24433f58186d625e5f4c
Jens Rasmussen <stamppot@gmail.com>
2011-11-27 23:18:51 +0100

    Flush memcache after deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -173,23 +173,23 @@ end
 ############### memcached recipes ##########
 namespace :memcached do 
 	desc "Start memcached"
-	task :start, :roles => [:app], :only => {:memcached => true} do
+	task :start, :roles => [:app] do
 		sudo "/etc/init.d/memcached start"
 	end
 	desc "Stop memcached"
-	task :stop, :roles => [:app], :only => {:memcached => true} do
+	task :stop, :roles => [:app] do
 		sudo "/etc/init.d/memcached stop"
 	end
 	desc "Restart memcached"
-	task :restart, :roles => [:app], :only => {:memcached => true} do
+	task :restart, :roles => [:app] do
 		sudo "/etc/init.d/memcached restart"
 	end        
 	desc "Flush memcached - this assumes memcached is on port 11211"
-	task :flush, :roles => [:app], :only => {:memcached => true} do
+	task :flush, :roles => [:app] do
 		sudo "echo 'flush_all' | nc localhost 11211"
 	end        
 	desc "Symlink the memcached.yml file into place if it exists"
-	task :symlink_configs, :roles => [:app], :only => {:memcached => true }, :except => { :no_release => true } do
+	task :symlink_configs, :roles => [:app], :except => { :no_release => true } do
 		run "if [ -f #{shared_path}/config/memcached.yml ]; then ln -nfs #{shared_path}/config/memcached.yml #{latest_release}/config/memcached.yml; fi"
 	end
 end
